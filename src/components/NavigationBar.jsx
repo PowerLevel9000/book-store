42e0f1c02abf4530c64fb2ff15ee530dee70b7ea
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-04 08:44:49 +0530

    navigation skeleton

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => (
-  <div>
-    <h1>
-      hello from Nvaigation
-    </h1>
-  </div>
+  <nav>
+    <div className="logo-wrapper"><Link to="/">Store</Link></div>
+    <ul className="menu">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/books">Books</Link></li>
+      <li><Link to="/catagories">Catagories</Link></li>
+    </ul>
+  </nav>
 );
 
 export default NavigationBar;
