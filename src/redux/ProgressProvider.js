4fbf1eba498e92f6343db92e119ee7877aeae722
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-11 15:03:48 +0530

    form styling

--- src/redux/ProgressProvider.js
+++ src/redux/ProgressProvider.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// If you don't have a version of React that supports
-// hooks, you can use a class-based version of this
-// component in ProgressProviderUsingClass.js
 const ProgressProvider = ({ valueStart, valueEnd, children }) => {
   const [value, setValue] = React.useState(valueStart);
   React.useEffect(() => {
